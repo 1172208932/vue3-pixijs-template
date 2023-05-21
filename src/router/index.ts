ad4c4806b97f7ce7bcf438057ec542810636bafa
zhang xiaoxu <1172208932@qq.com>
2023-05-21 20:30:38 +0800

    feat: add index

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,6 +3,11 @@ import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
+    name: "index",
+    component: () => import("../views/index/index.vue"),
+  },
+  {
+    path: "/game",
     name: "game",
     component: () => import("../views/game/index.vue"),
   }
---
+++ src/views/index/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="box">
+ 
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  reactive,
+  toRefs,
+  onMounted,
+  SetupContext,
+  ref,
+} from "vue";
+
+import { throttle } from "@/utils/throttle"
+export default defineComponent({
+  name: "index",
+  setup(props, { emit }: SetupContext) {
+    let mines = ref<number>(1);
+  
+
+    const state: {
+    } = reactive({
+    });
+
+    const begin = () => {
+     
+    };
+
+  
+    onMounted(async () => {
+    
+    });
+
+
+
+    return {
+      ...toRefs(state),
+     
+    };
+  },
+});
+</script>
+
+<style lang="scss">
+
+</style>
